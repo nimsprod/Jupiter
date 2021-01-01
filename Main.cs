d2a6d3670bb262810f4f1f72f130afdcecafb5aa
Jose Saez <jsaez@foreworth.com>
2021-01-01 10:51:07 +0100

    Created main

---
+++ Main.cs
@@ -0,0 +1,11 @@
+namespace AwesomeApplication
+{
+    public class Awesome
+    {
+        public void main()
+        {
+            Console.WriteLine("Hola");			
+			Console.WriteLine("Hola Mundo");	
+        }
+    }
+}
