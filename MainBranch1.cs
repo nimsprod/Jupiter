22bc86d1e62a293f714c06caea66102de296eaf8
Jose Saez <jsaez@foreworth.com>
2021-03-01 10:51:07 +0100

    Added MainBranch1 from branch2

---
+++ MainBranch1.cs
@@ -0,0 +1,13 @@
+namespace AwesomeApplication
+{
+    public class Awesome
+    {
+        public void main()
+        {
+            Console.WriteLine("Hola");			
+			Console.WriteLine("Hola Mundo");	
+			Console.WriteLine("Que tal?");	
+			Console.WriteLine("Bien");	
+        }
+    }
+}
